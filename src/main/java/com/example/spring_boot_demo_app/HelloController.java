42a007d4945611596f2b22d1c60217bc4437971f
aman <amankumarmaharjanaustralia@gmail.com>
2025-07-24 23:22:37 +1000

    First commit: Hello HelloController.java added

---
+++ src/main/java/com/example/spring_boot_demo_app/HelloController.java
@@ -0,0 +1,30 @@
+package com.example.spring_boot_demo_app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+    @GetMapping("/id")
+    public int getId() {
+        int id=1;
+        return id;
+    }
+    @GetMapping("/id/list")
+    public List<Integer> getIdList() {
+        List<Integer> idList=new ArrayList<>();
+        idList.add(1);
+        idList.add(2);
+        idList.add(3);
+        return idList;
+    }
+
+
+}
