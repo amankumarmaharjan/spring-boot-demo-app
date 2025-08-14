8e18f42d6b0bd2711aba3d17d7b9f6d36cb438dd
aman <amankumarmaharjanaustralia@gmail.com>
2025-08-14 20:35:30 +1000

    Constuctor overloading added more

--- src/main/java/com/example/spring_boot_demo_app/controller/ConstructorExampleController.java
+++ src/main/java/com/example/spring_boot_demo_app/controller/ConstructorExampleController.java
@@ -3,20 +3,18 @@ package com.example.spring_boot_demo_app.controller;
 import com.example.spring_boot_demo_app.model.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
-
 @RestController
 public class ConstructorExampleController {
- //constuctor sameclassname functiohn (object) creation only
+    //constuctor sameclassname functiohn (object) creation only
 //costructor overloading
 //tostring
 
     @GetMapping("/constructor/employee")
     public Employee getEmployee() {
 
-        Employee employee5=null;
+        Employee employee5 = null;
 
-        Employee employee1 = new Employee(1,"Arjun");
+        Employee employee1 = new Employee(1, "Arjun");
         Employee employee2 = new Employee(2);
         Employee employee3 = new Employee();
         Employee employee4 = new Employee(2, "aman", "maharjan");
@@ -24,7 +22,7 @@ public class ConstructorExampleController {
 //        employee5= new Employee(5,"XYZ");
         System.out.println(employee5);
         System.out.println("Employee 1: " + employee1);
-        System.out.println("Employee 2: " + employee2.getName()+ "id:"+employee2.getId());
+        System.out.println("Employee 2: " + employee2.getName() + "id:" + employee2.getId());
         System.out.println("Employee 3: " + employee3);
         System.out.println("Employee 4: " + employee4);
         System.out.println(1);
@@ -34,5 +32,22 @@ public class ConstructorExampleController {
         System.out.println(employee4);
 
         return employee4;
+    }
+
+    @PostMapping("/constructor/outsideEmployee")
+    public Employee getEmployeeByConsturctorOverLoading(@RequestBody Employee outsideEmployee) {
+
+        Employee employee;
+
+        if (outsideEmployee.getId() != 0 && outsideEmployee.getName() != null && outsideEmployee.getLastName() != null) {
+            employee = new Employee(2, "aman", "maharjan");
+        } else if (outsideEmployee.getId() != 0 && outsideEmployee.getName() != null) {
+            employee = new Employee(1, "Arjun");
+        } else if (outsideEmployee.getId() != 0) {
+            employee = new Employee(2);
+        } else {
+            employee = new Employee();
+        }
+        return employee;
     }
 }
